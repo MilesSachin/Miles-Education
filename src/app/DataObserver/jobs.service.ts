d65209cf59a95d2c5561d4b486045b317ce060ed
MilesSachin <sachin.singh@mileseducation.com>
2022-05-17 19:40:58 +0530

    data observable

--- src/app/DataObserver/jobs.service.ts
+++ src/app/DataObserver/jobs.service.ts
@@ -1,20 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobsService {
-  url:any='https://website-api.mileseducation.com/'
+  url: any = 'https://website-api.mileseducation.com/'
+  jobsListDataObserver = new Subject();
+  jobsListMetaDataObserver = new Subject();
+  locationListDataObserver: any = new Subject();
+  companyListDataObserver: any = new Subject();
 
-  constructor(private http:HttpClient) { }
-  jobs(param:any){
-    return this.http.post(this.url+'job/joblist',param);
+  constructor(private http: HttpClient) { }
+  jobs(param: any) {
+    this.http.post(this.url + 'job/joblist', param).subscribe((res: any) => {
+      this.jobsListDataObserver.next(res.data)
+    });
   }
-  location(param:any){
-    return this.http.post(this.url+'getJobsCityList',param);
+  location(param: any) {
+    this.http.post(this.url + 'getJobsCityList', param).subscribe((res: any) => {
+      this.locationListDataObserver.next(res.data)
+    });
   }
-  company(param:any){
-    return this.http.post(this.url+'getJobsCompanyList',param);
+  company(param: any) {
+    this.http.post(this.url + 'getJobsCompanyList', param).subscribe((res: any) => {
+      this.companyListDataObserver.next(res.data)
+    });
   }
 }
--- src/app/Module/jobs/jobs.component.ts
+++ src/app/Module/jobs/jobs.component.ts
@@ -9,12 +9,16 @@ import { JobsService } from 'src/app/DataObserver/jobs.service';
 })
 export class JobsComponent implements OnInit {
   myParam: any;
-  spocList: any;
-  location:any;
+  spocList: any=[];
+  location:any=[];
   company_img='https://cdn.mileseducation.com/MilesEducation-Images/company/logo_image/';
-  company: any;
+  company: any=[];
   selectedCompany: any;
   selectedLocation: any;
+  jobsListDataObserver = this.jobsService.jobsListDataObserver.asObservable();
+  locationListDataObserver = this.jobsService.locationListDataObserver.asObservable();
+  companyListDataObserver = this.jobsService.companyListDataObserver.asObservable();
+
   constructor(private route: ActivatedRoute, private jobsService: JobsService) { }
 
   ngOnInit(): void {
@@ -28,7 +32,6 @@ export class JobsComponent implements OnInit {
    var temp1 = this.selectedLocation?[this.selectedLocation]:[];
     var param = {
       filter: { 
-        
         qualification: this.myParam, 
         location: temp1, 
         company_name: temp, 
@@ -37,25 +40,26 @@ export class JobsComponent implements OnInit {
       limit: 12,
       page: 1
     }
-    this.jobsService.jobs(param).subscribe((res: any) => {
-      this.spocList = res.data;
-      // console.log(res.data);
+    this.jobsService.jobs(param)
+    this.jobsListDataObserver.subscribe((res: any) => {
+      this.spocList = res;
+      // console.log(res);
     });
   }
   locationList(){
     var param = {course: this.myParam}
-    this.jobsService.location(param).subscribe((res:any)=>{
-      this.location = res.data;
-      console.log(res.data);
-      
+    this.jobsService.location(param);
+    this.locationListDataObserver.subscribe((res:any)=>{
+      this.location = res;
+      // console.log(res);
     })
   }
   companyList(){
     var param = {course: this.myParam}
-    this.jobsService.company(param).subscribe((res:any)=>{
-      this.company = res.data;
-      console.log(res.data);
-      
+    this.jobsService.company(param);
+    this.companyListDataObserver.subscribe((res:any)=>{
+      this.company = res;
+      // console.log(res);
     })
   }
   
